99b71f18466386f046df793bc3de72b36800bf9b
fonluiz <luiz.fonseca@ccc.ufcg.edu.br>
2017-02-15 15:10:14 -0300

    Remove dead code from Near Stops Fragment

--- mobile/src/main/java/br/edu/ufcg/analytics/meliorbusao/fragments/NearStopsFragment.java
+++ mobile/src/main/java/br/edu/ufcg/analytics/meliorbusao/fragments/NearStopsFragment.java
@@ -5,7 +5,6 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.SearchView;
 import android.util.Log;
@@ -16,12 +15,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 
 import org.osmdroid.bonuspack.overlays.Marker;
@@ -41,161 +37,46 @@ import br.edu.ufcg.analytics.meliorbusao.adapters.InfoWindowAdapter;
 import br.edu.ufcg.analytics.meliorbusao.db.DBUtils;
 import br.edu.ufcg.analytics.meliorbusao.listeners.FragmentTitleChangeListener;
 import br.edu.ufcg.analytics.meliorbusao.listeners.OnMapInformationReadyListener;
-import br.edu.ufcg.analytics.meliorbusao.listeners.OnMeliorBusaoQueryListener;
 import br.edu.ufcg.analytics.meliorbusao.models.NearStop;
 import br.edu.ufcg.analytics.meliorbusao.models.Route;
-import br.edu.ufcg.analytics.meliorbusao.utils.ProgressUtils;
 
 
-public class NearStopsFragment extends Fragment implements OnMeliorBusaoQueryListener,
-        SearchView.OnQueryTextListener, OnMapInformationReadyListener {
-
-    private static NearStopsFragment instance;
-
-    private Menu mMenu;
-    private SearchView mSearchView;
-    private MapFragment mMapFragment;
-
-    private final double raio = 500;
-
-    private String address = "";
+public class NearStopsFragment extends Fragment implements SearchView.OnQueryTextListener,
+        OnMapInformationReadyListener {
 
     public static final String TAG = "NearStopsFragment";
-    private BitmapDescriptor mParadaBitmap;
-    private ProgressBar progressSpinner;
+    private static final double RAIO = 500;
+    private static NearStopsFragment instance;
+    private MapFragment mMapFragment;
+    private Menu mMenu;
     private NearStopListener mCallback;
+    private SearchView mSearchView;
+
+    public NearStopsFragment() {
+    }
 
     public static NearStopsFragment getInstance() {
         if (instance == null) {
             NearStopsFragment fragment = new NearStopsFragment();
-            Bundle args = new Bundle();
-            fragment.setArguments(args);
             instance = fragment;
         }
         return instance;
     }
 
-    public NearStopsFragment() {
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         mMapFragment = new MapFragment();
         mMapFragment.setOnMapInformationReadyListener(this);
+        setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View mView = inflater.inflate(R.layout.fragment_near_stops, container, false);
         getChildFragmentManager().beginTransaction().replace(R.id.near_stops_map_fragment, mMapFragment).commit();
-
-        progressSpinner = ProgressUtils.buildProgressBar(getContext());
-
-        setHasOptionsMenu(true);
-
         return mView;
     }
-
-
-    /**
-     * Add a marker in the map to each bus stop that is located within a certain diameter.
-     * @param centerPoint The center of the diameter.
-     */
-    private void loadNearStops(GeoPoint centerPoint) {
-        TreeSet<NearStop> paradas = DBUtils.getNearStops(getContext(), centerPoint.getLatitude(),
-                centerPoint.getLongitude(), raio, null);
-        for (NearStop parada : paradas) {
-            Marker stopMarker = mMapFragment.addMarker(new GeoPoint(parada.getLatitude(), parada.getLongitude()));
-            stopMarker.setIcon(getResources().getDrawable(R.drawable.ic_bus_stop_sign));
-            stopMarker.setInfoWindow(new NearStopMarkerInfoWindow(mMapFragment.getMapView(), parada));
-        }
-    }
-
-
-    private void setAddressName(double lat, double lon) {
-        Geocoder geocoder;
-        List<Address> addresses;
-        geocoder = new Geocoder(getContext(), Locale.getDefault());
-
-        try {
-            addresses = geocoder.getFromLocation(lat, lon, 1);
-            setAddress(addresses.get(0).getAddressLine(0));
-
-        } catch (Exception e) {
-            Log.e("Erro address", e.getMessage());
-        }
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    private CircleOptions getCircleOptions(LatLng point) {
-        CircleOptions circleOptions = new CircleOptions();
-        circleOptions.radius(raio);
-        circleOptions.fillColor(ContextCompat.getColor(getContext(), R.color.nearStopsAreaFill));
-        circleOptions.strokeColor(ContextCompat.getColor(getContext(), R.color.nearStopsAreaStroke));
-        circleOptions.strokeWidth(3);
-        circleOptions.center(new LatLng(point.latitude, point.longitude));
-        return circleOptions;
-    }
-
-    @Override
-    public void onMeliorBusaoQueryChange(String query) {
-
-    }
-
-    @Override
-    public boolean onMeliorBusaoQuerySubmit(String query) {
-        try {
-            address = query;
-            try {
-                Geocoder geocoder;
-                geocoder = new Geocoder(getContext(), Locale.getDefault());
-                LatLng point = new LatLng(geocoder.getFromLocationName(address + "," + Constants.CITY, 1).get(0).getLatitude(),
-                        geocoder.getFromLocationName(address + "," + Constants.CITY, 1).get(0).getLongitude());
-                List<Address> addresses;
-                addresses = geocoder.getFromLocation(point.latitude, point.longitude, 1);
-                if (addresses.get(0).getLocality().compareTo(Constants.CITY) == 0) {
-                    setAddressName(point.latitude, point.longitude);
-                    return true;
-                } else {
-                    Toast.makeText(getActivity(), getString(R.string.msg_unable_to_find_route), Toast.LENGTH_LONG).show();
-                }
-            } catch (Exception e) {
-                Log.e("NearStopsFragment", e.getMessage());
-            }
-
-        } catch (Exception e) {
-            Log.e("NearStopsFragment", "Não foi possível achar a localizacao: " + e.getMessage());
-            Toast.makeText(getActivity(), getString(R.string.msg_failed_detect_location), Toast.LENGTH_SHORT).show();
-        }
-        return false;
-    }
-
-    @Override
-    public void onMapAddressFetched(String mapAddres) {
-
-    }
-
-    @Override
-    public void onMapLocationAvailable(Location mapLocation) {
-        loadNearStops(new GeoPoint(mapLocation.getLatitude(), mapLocation.getLongitude()));
-    }
-
-    @Override
-    public void onMapClick(GeoPoint geoPoint) {
-        mMapFragment.clearMap();
-        mMapFragment.updatePlaceMarker(geoPoint);
-        loadNearStops(geoPoint);
-    }
-
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
@@ -213,11 +94,57 @@ public class NearStopsFragment extends Fragment implements OnMeliorBusaoQueryLis
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        ((MelhorBusaoActivity) getActivity()).getSupportActionBar()
+                .setTitle(R.string.near_stops_title);
+    }
+
+    public void addNearStopListener(NearStopListener callback) {
+        mCallback = callback;
+    }
+
+    /**
+     * Add a marker in the map to each bus stop that is located within a certain diameter.
+     * @param centerPoint The center of the diameter.
+     */
+    private void loadNearStops(GeoPoint centerPoint) {
+        TreeSet<NearStop> nearStops = DBUtils.getNearStops(getContext(), centerPoint.getLatitude(),
+                centerPoint.getLongitude(), RAIO, null);
+        for (NearStop stop : nearStops) {
+            Marker stopMarker = mMapFragment.addMarker(new GeoPoint(stop.getLatitude(), stop.getLongitude()));
+            stopMarker.setIcon(getResources().getDrawable(R.drawable.ic_bus_stop_sign));
+            stopMarker.setInfoWindow(new NearStopMarkerInfoWindow(mMapFragment.getMapView(), stop));
+        }
+    }
+
+
+    @Override
+    public void onMapAddressFetched(String mapAddres) {
+    }
+
+    @Override
+    public void onMapLocationAvailable(Location mapLocation) {
+        loadNearStops(new GeoPoint(mapLocation.getLatitude(), mapLocation.getLongitude()));
+    }
+
+    @Override
+    public void onMapClick(GeoPoint geoPoint) {
+        mMapFragment.clearMap();
+        mMapFragment.updatePlaceMarker(geoPoint);
+        loadNearStops(geoPoint);
+    }
+
+    /**
+     * Mark a new place in the map according to the addres passed if the address exists.
+     * @param query The addres for looking up.
+     * @return If the address was successfully found or not.
+     */
+    @Override
     public boolean onQueryTextSubmit(String query) {
         if (!((MelhorBusaoActivity) getActivity()).checkInternetConnection()) {
             Toast.makeText(getContext(), getString(R.string.msg_search_needs_internet), Toast.LENGTH_LONG).show();
             return false;
-
         } else {
             try {
                 Geocoder geocoder;
@@ -237,19 +164,11 @@ public class NearStopsFragment extends Fragment implements OnMeliorBusaoQueryLis
                     Toast.makeText(getActivity(), getString(R.string.msg_failed_locate_search), Toast.LENGTH_LONG).show();
                 }
             } catch (Exception e) {
-                Log.e("NearStopsFragment", e.getMessage());
+                Log.e(TAG, e.getMessage());
             }
 
         }
         return false;
-    }
-
-    public void addNearStopListener(NearStopListener callback) {
-        mCallback = callback;
-    }
-
-    public interface NearStopListener extends FragmentTitleChangeListener {
-        void onInfoWindowClick(HashSet<Route> routes, String stopName);
     }
 
     @Override
@@ -257,6 +176,16 @@ public class NearStopsFragment extends Fragment implements OnMeliorBusaoQueryLis
         return true;
     }
 
+    /**
+     * Listener for NearStopFragment
+     */
+    public interface NearStopListener extends FragmentTitleChangeListener {
+        void onInfoWindowClick(HashSet<Route> routes, String stopName);
+    }
+
+    /**
+     * Class representing the info window that shows up when clicking in a marker.
+     */
     class NearStopMarkerInfoWindow extends MarkerInfoWindow {
 
         private NearStop mStop;
