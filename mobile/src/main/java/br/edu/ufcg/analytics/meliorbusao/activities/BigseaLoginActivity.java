711b7cee0472ac05182e3f612a1c9c4e5eea3663
fonluiz <luiz.fonseca@ccc.ufcg.edu.br>
2017-04-12 16:18:04 -0300

    Refactor bigsea login class

--- mobile/src/main/java/br/edu/ufcg/analytics/meliorbusao/activities/BigseaLoginActivity.java
+++ mobile/src/main/java/br/edu/ufcg/analytics/meliorbusao/activities/BigseaLoginActivity.java
@@ -5,49 +5,32 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -55,54 +38,31 @@ import br.edu.ufcg.analytics.meliorbusao.R;
 
 
 /**
- * A login screen that offers login via email/password.
+ * A isSuccessfulLogin screen that offers isSuccessfulLogin via email/password.
  */
-public class BigseaLoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+public class BigseaLoginActivity extends AppCompatActivity {
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
+    public static final String TAG = "BigSeaLoginActivity";
+    // Keep track of the isSuccessfulLogin task to ensure we can cancel it if requested.
     private UserLoginTask mAuthTask = null;
-
     // UI references.
-    private AutoCompleteTextView mEmailView;
+    private AutoCompleteTextView mUsernameView;
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
-
-    public static final String TAG = "BIGSEA_LOGIN_ACTIVITY";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bigsea_login);
         getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
 
+        // Set up the isSuccessfulLogin form.
+        mUsernameView = (AutoCompleteTextView) findViewById(R.id.username);
         mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
-                    return true;
-                }
-                return false;
-            }
-        });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+        Button mSignInButton = (Button) findViewById(R.id.sign_in_button);
+        mSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 attemptLogin();
@@ -111,18 +71,12 @@ public class BigseaLoginActivity extends AppCompatActivity implements LoaderCall
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
-
     }
-
-    private void populateAutoComplete() {
-        getLoaderManager().initLoader(0, null, this);
-    }
-
 
     /**
-     * Attempts to sign in or register the account specified by the login form.
+     * Attempts to sign in or register the account specified by the isSuccessfulLogin form.
      * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
+     * errors are presented and no actual isSuccessfulLogin attempt is made.
      */
     private void attemptLogin() {
         if (mAuthTask != null) {
@@ -130,58 +84,60 @@ public class BigseaLoginActivity extends AppCompatActivity implements LoaderCall
         }
 
         // Reset errors.
-        mEmailView.setError(null);
+        mUsernameView.setError(null);
         mPasswordView.setError(null);
 
-        // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString();
+        // Store values at the time of the isSuccessfulLogin attempt.
+        String username = mUsernameView.getText().toString();
         String password = mPasswordView.getText().toString();
 
         boolean cancel = false;
 
-        // There was an error; don't attempt login and focus the first
+        // There was an error; don't attempt isSuccessfulLogin and focus the first
         // form field with an error.
-
         // Check for a valid password, if the user entered one.
-        if (TextUtils.isEmpty(password) || !isPasswordValid(password)) {
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_field_required));
+            mPasswordView.requestFocus();
+            cancel = true;
+        } else if(!isPasswordValid(password)) {
             mPasswordView.setError(getString(R.string.error_incorrect_password));
             mPasswordView.requestFocus();
             cancel = true;
         }
 
-        // Check for a valid email address.
-        if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            mEmailView.requestFocus();
+        // Check for a valid username.
+        if (TextUtils.isEmpty(username)) {
+            mUsernameView.setError(getString(R.string.error_field_required));
+            mUsernameView.requestFocus();
             cancel = true;
-        } /*else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            mEmailView.requestFocus();
+        } else if (!isUsernameValid(username)) {
+            mUsernameView.setError(getString(R.string.error_invalid_username));
+            mUsernameView.requestFocus();
             cancel = true;
-        }*/
+        }
 
-
-         if(!cancel) {
+        if (!cancel) {
             // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
+            // perform the user isSuccessfulLogin attempt.
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask = new UserLoginTask(username, password);
             mAuthTask.execute((Void) null);
         }
     }
 
-    private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
-        return email.contains("@");
+    private boolean isUsernameValid(String username) {
+        return username.length() >= 3 &&
+                username.length() <= 15;
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
+        return password.length() >= 5 &&
+                password.length() <= 25;
     }
 
     /**
-     * Shows the progress UI and hides the login form.
+     * Shows the progress UI and hides the isSuccessfulLogin form.
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
     private void showProgress(final boolean show) {
@@ -216,86 +172,31 @@ public class BigseaLoginActivity extends AppCompatActivity implements LoaderCall
         }
     }
 
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
-                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-                // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                .CONTENT_ITEM_TYPE},
-
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
-                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        List<String> emails = new ArrayList<>();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            emails.add(cursor.getString(ProfileQuery.ADDRESS));
-            cursor.moveToNext();
-        }
-
-        addEmailsToAutoComplete(emails);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-
-    }
-
-    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(BigseaLoginActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-        mEmailView.setAdapter(adapter);
-    }
-
-
-    private interface ProfileQuery {
-        String[] PROJECTION = {
-                ContactsContract.CommonDataKinds.Email.ADDRESS,
-                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-        };
-
-        int ADDRESS = 0;
-        int IS_PRIMARY = 1;
-    }
 
     /**
-     * Represents an asynchronous login/registration task used to authenticate
+     * Represents an asynchronous isSuccessfulLogin/registration task used to authenticate
      * the user.
      */
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
-        private final String userName;
-        private final String mPassword;
+        private static final String ENDPOINT_ADDRESS = "https://eubrabigsea.dei.uc.pt/engine/api/checkin_data";
+        private final String username;
+        private final String password;
         private String responseMessage = "";
-        private boolean login;
+        private boolean isSuccessfulLogin;
 
-        UserLoginTask(String userName, String password) {
-            this.userName = userName;
-            this.mPassword = password;
-
+        UserLoginTask(String username, String password) {
+            this.username = username;
+            this.password = password;
         }
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
-            URL url = null;
+            URL url;
             try {
-                String parameters = "user=" + userName + "&pwd=" +mPassword;
+                String parameters = "user=" + username + "&pwd=" + password;
+                url = new URL(ENDPOINT_ADDRESS);
 
-                url = new URL("https://eubrabigsea.dei.uc.pt/engine/api/checkin_data");
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("POST");
 
@@ -307,28 +208,20 @@ public class BigseaLoginActivity extends AppCompatActivity implements LoaderCall
                 os.close();
 
                 conn.connect();
-                int responseCode=conn.getResponseCode();
+                int responseCode = conn.getResponseCode();
 
                 if (responseCode == HttpsURLConnection.HTTP_OK) {
                     String line = "";
-                    BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    while ((line=br.readLine()) != null) {
-                        responseMessage+=line;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    while ((line = br.readLine()) != null) {
+                        responseMessage += line;
                     }
-
                     JSONObject jsonObject = new JSONObject(responseMessage);
-                    if (jsonObject.getBoolean("success")){
-                        login = true;
-                    } else {
-                        login = false;
-                    }
+                    isSuccessfulLogin = jsonObject.getBoolean("success");
+                } else {
+                    responseMessage = "";
+                    isSuccessfulLogin = false;
                 }
-
-                else {
-                    responseMessage="";
-                    login = false;
-                }
-
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -337,9 +230,6 @@ public class BigseaLoginActivity extends AppCompatActivity implements LoaderCall
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-
-
-            // TODO: register the new account here.
             return true;
         }
 
@@ -348,23 +238,18 @@ public class BigseaLoginActivity extends AppCompatActivity implements LoaderCall
             mAuthTask = null;
             showProgress(false);
 
-            if (login) {
-
+            if (isSuccessfulLogin) {
                 final Intent i = new Intent(BigseaLoginActivity.this, MelhorBusaoActivity.class);
                 startActivity(i);
                 View view = getCurrentFocus();
                 InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-
                 finish();
-
             } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
+                mUsernameView.setError(getString(R.string.error_unsuccessful_login));
+                mUsernameView.requestFocus();
             }
-
             Log.d(TAG, responseMessage);
-
         }
 
         @Override
