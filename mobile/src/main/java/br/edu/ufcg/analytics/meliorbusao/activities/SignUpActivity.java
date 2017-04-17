3ecb369b0bdca6c0c42f10c6a27e2af76c3be314
rafaelle <rafaelle.xavier@gmail.com>
2017-04-17 10:31:05 -0300

    Starting 'sign up'

--- mobile/src/main/java/br/edu/ufcg/analytics/meliorbusao/activities/SignUpActivity.java
+++ mobile/src/main/java/br/edu/ufcg/analytics/meliorbusao/activities/SignUpActivity.java
@@ -1,15 +1,279 @@
 package br.edu.ufcg.analytics.meliorbusao.activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import br.edu.ufcg.analytics.meliorbusao.Constants;
 import br.edu.ufcg.analytics.meliorbusao.R;
+import br.edu.ufcg.analytics.meliorbusao.utils.SharedPreferencesUtils;
+
+import static android.R.attr.name;
 
 public class SignUpActivity extends AppCompatActivity {
+    public static final String TAG = "SIGN_UP_ACTIVITY";
+
+    private UserRegisterTask mAuthTask = null;
+
+    private AutoCompleteTextView mFirstNameView;
+    private AutoCompleteTextView mLastNameView;
+    private AutoCompleteTextView mEmailView;
+    private AutoCompleteTextView mUserNameView;
+    private AutoCompleteTextView mPasswordView;
+    private AutoCompleteTextView mPasswordConfirmationView;
+    private Button mSignupButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mFirstNameView = (AutoCompleteTextView) findViewById(R.id.first_name);
+        mLastNameView = (AutoCompleteTextView) findViewById(R.id.last_name);
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        mUserNameView = (AutoCompleteTextView) findViewById(R.id.username);
+        mPasswordView = (AutoCompleteTextView) findViewById(R.id.password);
+        mPasswordConfirmationView = (AutoCompleteTextView) findViewById(R.id.password_confirmation);
+
+        mSignupButton = (Button) findViewById(R.id.signup_button);
+
+
+        mSignupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        //mLoginFormView = findViewById(R.id.login_form);
+        //mProgressView = findViewById(R.id.login_progress);
+    }
+
+
+    /**
+     * Attempts to register the account specified by the isSuccessfulRegister form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual isSuccessfulRegister attempt is made.
+     */
+
+    private void attemptLogin() {
+
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mFirstNameView.setError(null);
+        mLastNameView.setError(null);
+        mEmailView.setError(null);
+        mUserNameView.setError(null);
+        mPasswordView.setError(null);
+        mPasswordConfirmationView.setError(null);
+
+        boolean cancel = false;
+
+
+        // There was an error; don't attempt isSuccessfulRegister and focus the first
+        // form field with an error.
+        cancel = isNameInvalid(mFirstNameView) && isNameInvalid(mLastNameView) &&
+                isEmailInValid(mEmailView) && isNameInvalid(mUserNameView) && isPasswordInvalid(mPasswordView);
+
+
+            if (!cancel) {
+                // Show a progress spinner, and kick off a background task to
+                // perform the user isSuccessfulLogin attempt.
+               // mAuthTask = new UserRegisterTask(username, password);
+                //mAuthTask.execute((Void) null);
+            }
+
+
+    }
+
+    private boolean isNameInvalid(AutoCompleteTextView name) {
+        boolean invalid = false;
+
+        if (name==null || name.getText().equals("")) {
+            name.setError(getString(R.string.error_field_required));
+            invalid = true;
+        } else if (name.getText().length() < 3 || name.getText().length() > 15) {
+            if (mUserNameView!= null && name.getHint().equals(mUserNameView.getHint())){
+                name.setError(getString(R.string.userName_restriction));
+            } else {
+                name.setError(name.getHint() + " " + getString(R.string.name_restriction));
+            }
+            invalid = true;
+        }else if (!name.getText().toString().matches(".*\\d.*")){
+            name.setError(name.getHint() + " " + getString(R.string.name_restriction_no_number));
+            invalid= true;
+        }
+
+        if (invalid){
+            name.requestFocus();
+        }
+        return  invalid;
+    }
+
+
+    private boolean isEmailInValid(AutoCompleteTextView email) {
+
+        boolean invalid = false;
+
+        if (email==null || email.getText().equals("")) {
+            email.setError(getString(R.string.error_field_required));
+            invalid = true;
+        }else if (!email.getText().toString().contains("@")){
+            //TODO: Replace this with your own logic
+            email.setError(getString(R.string.email_restriction));
+            invalid= true;
+        }
+
+        if (invalid){
+            email.requestFocus();
+        }
+        return  invalid;
+
+    }
+
+
+
+    private boolean isPasswordInvalid(AutoCompleteTextView password){
+        boolean invalid = false;
+
+        if (password==null || password.equals("")) {
+            mPasswordView.setError(getString(R.string.error_field_required));
+            invalid = true;
+        } else if (password.getText().length() < 5 || password.getText().length() > 25){
+            mPasswordView.setError(getString(R.string.password_error));
+            //Passwords must match. Needs to be between 5 and 25 characters. Case sensitive. No special characters allowed.
+            invalid = true;
+        } else if (!password.getText().toString().matches(".*\\d.*")){
+            mPasswordView.setError(getString(R.string.password_restriction));
+            invalid= true;
+        }
+
+        if (invalid){
+            password.requestFocus();
+        }
+        return  invalid;
+    }
+
+
+
+
+
+    /**
+     * Represents an asynchronous isSuccessfulLogin/registration task used to authenticate
+     * the user.
+     */
+    public class UserRegisterTask extends AsyncTask<Void, Void, Boolean> {
+
+        private static final String ENDPOINT_ADDRESS = "https://eubrabigsea.dei.uc.pt/engine/api/checkin_data";
+        private final String username;
+        private final String password;
+        private String responseMessage = "";
+        private boolean isSuccessfulLogin;
+        private String token;
+        private String loginServiceType;
+
+        UserRegisterTask(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            /*
+            URL url;
+            try {
+                String parameters = "user=" + username + "&pwd=" + password;
+                url = new URL(ENDPOINT_ADDRESS);
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                writer.write(parameters);
+                writer.flush();
+                writer.close();
+                os.close();
+
+                conn.connect();
+                int responseCode = conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                    String line = "";
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    while ((line = br.readLine()) != null) {
+                        responseMessage += line;
+                    }
+                    JSONObject jsonObject = new JSONObject(responseMessage);
+                    isSuccessfulLogin = jsonObject.getBoolean("success");
+
+                    if (isSuccessfulLogin){
+                        JSONObject userInfo = new JSONObject(jsonObject.getString("user_info"));
+                        token = userInfo.getString("user_token");
+                        loginServiceType = Constants.BIG_SEA_SERVICE;
+                    }
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } */
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            mAuthTask = null;
+/*
+            if (isSuccessfulLogin) {
+                SharedPreferencesUtils.setUserToken(getApplicationContext(), loginServiceType, token);
+                final Intent i = new Intent(SignUpActivity.this, MelhorBusaoActivity.class);
+                startActivity(i);
+                View view = getCurrentFocus();
+                InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                finish();
+            } else {
+                //mUsernameView.setError(getString(R.string.error_unsuccessful_login));
+                //mUsernameView.requestFocus();
+            }
+            Log.d(TAG, responseMessage);
+            */
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+        }
     }
 }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
