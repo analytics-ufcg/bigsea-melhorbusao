6417033b9d88411cb74276fb9ea9d7c374a4dbbb
Tarciso Braz <tarcisocomp@gmail.com>
2017-05-01 16:31:41 -0300

    Using current location map-clicked location as origin/destination in OpenTripPlanner Directions API call.

--- mobile/src/main/java/br/edu/ufcg/analytics/meliorbusao/fragments/SearchScheduleFragment.java
+++ mobile/src/main/java/br/edu/ufcg/analytics/meliorbusao/fragments/SearchScheduleFragment.java
@@ -1,16 +1,10 @@
 package br.edu.ufcg.analytics.meliorbusao.fragments;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.app.Activity;
 import android.content.Context;
-import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.MenuItemCompat;
@@ -21,7 +15,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -36,39 +29,27 @@ import org.json.JSONObject;
 import org.osmdroid.util.GeoPoint;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
-import java.util.Set;
-import java.util.TreeSet;
 
 import javax.net.ssl.HttpsURLConnection;
 
-import br.edu.ufcg.analytics.meliorbusao.Constants;
 import br.edu.ufcg.analytics.meliorbusao.R;
 import br.edu.ufcg.analytics.meliorbusao.activities.MelhorBusaoActivity;
 import br.edu.ufcg.analytics.meliorbusao.adapters.RoutesAdapter;
 import br.edu.ufcg.analytics.meliorbusao.adapters.StopsAdapter;
-import br.edu.ufcg.analytics.meliorbusao.db.DBUtils;
 import br.edu.ufcg.analytics.meliorbusao.listeners.FragmentTitleChangeListener;
 import br.edu.ufcg.analytics.meliorbusao.listeners.OnMapInformationReadyListener;
 import br.edu.ufcg.analytics.meliorbusao.listeners.OnStopTimesReadyListener;
-import br.edu.ufcg.analytics.meliorbusao.models.NearStop;
-import br.edu.ufcg.analytics.meliorbusao.models.Route;
-import br.edu.ufcg.analytics.meliorbusao.models.Stop;
 import br.edu.ufcg.analytics.meliorbusao.models.StopHeadsign;
 import br.edu.ufcg.analytics.meliorbusao.models.StopTime;
-import br.edu.ufcg.analytics.meliorbusao.utils.ParseUtils;
-import br.edu.ufcg.analytics.meliorbusao.utils.StopRouteUtils;
+import br.edu.ufcg.analytics.meliorbusao.utils.SharedPreferencesUtils;
 
 public class SearchScheduleFragment extends Fragment implements OnStopTimesReadyListener,
         SearchView.OnQueryTextListener, OnMapInformationReadyListener {
@@ -77,8 +58,9 @@ public class SearchScheduleFragment extends Fragment implements OnStopTimesReady
     public static final String SELECTED_ROUTE_KEY = "selectedRouteKey";
     private static SearchScheduleFragment instance;
     private ArrayList<StopHeadsign> paradasDisponiveis;
-    private Button showScheduleButton;
-    private LatLng mCoordinates;
+    private Button getDirectionsButton;
+    private LatLng mDestLocation;
+    private LatLng mCurrLocation;
     private MapFragment mMapFragment;
     private Menu mMenu;
     private RoutesAdapter mRoutesAdapter;
@@ -120,36 +102,22 @@ public class SearchScheduleFragment extends Fragment implements OnStopTimesReady
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View mView = inflater.inflate(R.layout.fragment_search_schedule, container, false);
-        getChildFragmentManager().beginTransaction().replace(R.id.melhor_map_fragment, mMapFragment).commit();
-
-        paradasDisponiveis = new ArrayList<>();
-        initializeRoutesSpinner(mView);
-
-        stopsSpinner = (Spinner) mView.findViewById(R.id.schedules_stops_spinner);
-        mStopsAdapter = new StopsAdapter(getActivity(), new ArrayList<StopHeadsign>(), null);
-        stopsSpinner.setAdapter(mStopsAdapter);
+        View mView = inflater.inflate(R.layout.fragment_get_directions, container, false);
+        getChildFragmentManager().beginTransaction().replace(R.id.get_directions_map_fragment, mMapFragment).commit();
 
         addressField = (TextView) mView.findViewById(R.id.address_field);
 
         /*mProgressView = getActivity().findViewById(R.id.login_progress);*/
 
-        showScheduleButton = (Button) mView.findViewById(R.id.show_schedule_button);
-        showScheduleButton.setEnabled(false);
-        showScheduleButton.setBackgroundColor(getResources().getColor(R.color.inactiveIconColorDark));
-        showScheduleButton.setOnClickListener(new View.OnClickListener() {
+        getDirectionsButton = (Button) mView.findViewById(R.id.show_schedule_button);
+//        getDirectionsButton.setEnabled(false);
+        getDirectionsButton.setBackgroundColor(getResources().getColor(R.color.inactiveIconColorDark));
+        getDirectionsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 /*showProgress(true);*/
-                mAuthTask = new RoutingTask(-25.39211,-49.22613,-25.45102,-49.28381);
+                mAuthTask = new RoutingTask(mCurrLocation, mDestLocation);
                 mAuthTask.execute();
-                /*try {
-                    StopHeadsign stop = ((StopHeadsign) stopsSpinner.getSelectedItem());
-                    mCallback.onClickTakeBusButton(stop);
-                } catch (Exception e) {
-                    Toast.makeText(getActivity(), R.string.msg_no_stops_near, Toast.LENGTH_LONG).show();
-                }*/
-
             }
         });
 
@@ -173,7 +141,7 @@ public class SearchScheduleFragment extends Fragment implements OnStopTimesReady
     @Override
     public void onStart() {
         super.onStart();
-        ((MelhorBusaoActivity) getActivity()).getSupportActionBar().setTitle(R.string.bus_schedule_title);
+        ((MelhorBusaoActivity) getActivity()).getSupportActionBar().setTitle(R.string.get_directions_title);
         if (!((MelhorBusaoActivity) getActivity()).isLocationEnabled()) {
             ((MelhorBusaoActivity) getActivity()).buildAlertMessageNoGps();
         }
@@ -189,80 +157,9 @@ public class SearchScheduleFragment extends Fragment implements OnStopTimesReady
         }
     }
 
-    /**
-     * Initialize the spinner that shows nearby routes.
-     * @param mView The container view of the spinner
-     */
-    private void initializeRoutesSpinner(View mView) {
-        routesSpinner = (Spinner) mView.findViewById(R.id.schedules_routes_spinner);
-        mRoutesAdapter = new RoutesAdapter(getActivity(), new ArrayList<Route>());
-        routesSpinner.setAdapter(mRoutesAdapter);
-        routesSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                paradasDisponiveis.clear();
-                Route selectedRoute = mRoutesAdapter.getItem(position);
-                Set<Stop> paradasDaRota = DBUtils.getParadasRota(getContext(), selectedRoute);
-                Set<NearStop> nearbyStops = findNearbyStops(new LatLng(mCoordinates.latitude, mCoordinates.longitude));
-                for (NearStop nearStop : nearbyStops) {
-                    if (paradasDaRota.contains(nearStop)) {
-                        ParseUtils.getStopTime(selectedRoute, nearStop, SearchScheduleFragment.this);
-                    }
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-    }
-
-    /**
-     * Find routes that pass within a five hundred meters radius from the coordinates.
-     * @param coordinates The central point for calculating the area.
-     * @return A List with all the nearby routes.
-     */
-    private List<Route> findNearbyRoutes(LatLng coordinates) {
-        Set<NearStop> nearbyStops = findNearbyStops(coordinates);
-        Set<Route> availableRoutes = StopRouteUtils.getRoutesFromStops(new TreeSet<>(nearbyStops));
-        List<Route> nearbyRoutes = new ArrayList<>(availableRoutes);
-        Collections.sort(nearbyRoutes);
-        return nearbyRoutes;
-    }
-
-    /**
-     * Find bus stops located within a five hundred meters radius from the given coordinates.
-     * @param coordinates The central point for calculating the area.
-     * @return A List with all the nearby stops.
-     */
-    private Set<NearStop> findNearbyStops(LatLng coordinates) {
-        Set<NearStop> nearbyStops = new TreeSet<>(DBUtils.getNearStops(getContext(), coordinates.latitude,
-                coordinates.longitude, Constants.NEAR_STOPS_RADIUS, null));
-        return  nearbyStops;
-    }
-
-    /**
-     * Executes an action every time a StopHeadSignObj is loaded from the server. The StopHeadSignObjs
-     * are loaded each one at a time for each nearby stop for the selected nearby route.
-     *
-     * @param stopHeadsignObj The object loaded by the server.
-     * @param e An exception that might have been thrown.
-     */
     @Override
     public void onStopHeadsignReady(StopHeadsign stopHeadsignObj, ParseException e) {
-        paradasDisponiveis.add(stopHeadsignObj);
-        if (paradasDisponiveis.isEmpty()) {
-            stopsSpinner.setBackgroundColor(Color.parseColor("#F3F3F3"));
-            showScheduleButton.setEnabled(false);
-            showScheduleButton.setBackgroundColor(Color.parseColor("#DCDCDC"));
-        } else {
-            showScheduleButton.setEnabled(true);
-            showScheduleButton.setTextColor(Color.parseColor("#F4F4FA"));
-            showScheduleButton.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-        }
-        mStopsAdapter.clear();
-        mStopsAdapter.addAll(paradasDisponiveis);
-        mStopsAdapter.notifyDataSetChanged();
+
     }
 
     @Override
@@ -283,18 +180,14 @@ public class SearchScheduleFragment extends Fragment implements OnStopTimesReady
             try {
                 Geocoder geocoder;
                 geocoder = new Geocoder(getContext(), Locale.getDefault());
-                mCoordinates = new LatLng(geocoder.getFromLocationName(query + "," + Constants.CITY, 1).get(0).getLatitude(),
-                        geocoder.getFromLocationName(query + "," + Constants.CITY, 1).get(0).getLongitude());
+                String cityName = SharedPreferencesUtils.getCityNameOnDatabase(getContext());
+                Address queryResultAddress =  geocoder.getFromLocationName(query + "," + cityName, 1).get(0);
+                mDestLocation = new LatLng(queryResultAddress.getLatitude(),queryResultAddress.getLongitude());
                 List<Address> addresses;
-                addresses = geocoder.getFromLocation(mCoordinates.latitude, mCoordinates.longitude, 1);
-                if (addresses.get(0).getLocality().compareTo(Constants.CITY) == 0) {
-                    mMapFragment.updatePlaceMarker(new GeoPoint(mCoordinates.latitude, mCoordinates.longitude));
+                addresses = geocoder.getFromLocation(mDestLocation.latitude, mDestLocation.longitude, 1);
+                if (addresses.get(0).getLocality().compareTo(cityName) == 0) {
+                    mMapFragment.updatePlaceMarker(new GeoPoint(mDestLocation.latitude, mDestLocation.longitude));
                     mMenu.findItem(R.id.action_search).collapseActionView();
-                    List<Route> nearbyRoutes = findNearbyRoutes(mCoordinates);
-                    mRoutesAdapter.clear();
-                    mRoutesAdapter.addAll(nearbyRoutes);
-                    mRoutesAdapter.notifyDataSetChanged();
-                    routesSpinner.setAdapter(mRoutesAdapter);
                     return true;
                 } else {
                     Toast.makeText(getActivity(), R.string.msg_failed_locate_search, Toast.LENGTH_LONG).show();
@@ -321,22 +214,7 @@ public class SearchScheduleFragment extends Fragment implements OnStopTimesReady
      */
     @Override
     public void onMapLocationAvailable(Location mapLocation) {
-        mCoordinates = new LatLng(mapLocation.getLatitude(), mapLocation.getLongitude());
-        List<Route> nearbyRoutes = findNearbyRoutes(mCoordinates);
-        mRoutesAdapter.clear();
-        mRoutesAdapter.addAll(nearbyRoutes);
-        mRoutesAdapter.notifyDataSetChanged();
-
-        String selectedRouteName = (String) this.getArguments().get(SELECTED_ROUTE_KEY);
-        if (selectedRouteName != null) {
-            int index = mRoutesAdapter.getRouteIndexByName(selectedRouteName);
-            if (index < 0){
-                Toast.makeText(getContext(), R.string.msg_no_stops_for_bus, Toast.LENGTH_LONG).show();
-            } else {
-                routesSpinner.setSelection(index);
-            }
-        }
-        this.getArguments().putString(SELECTED_ROUTE_KEY, null);
+        mCurrLocation = new LatLng(mapLocation.getLatitude(), mapLocation.getLongitude());
     }
 
     /**
@@ -345,12 +223,7 @@ public class SearchScheduleFragment extends Fragment implements OnStopTimesReady
      */
     @Override
     public void onMapClick(GeoPoint geoPoint) {
-        mCoordinates = new LatLng(geoPoint.getLatitude(), geoPoint.getLongitude());
-        List<Route> nearbyRoutes = findNearbyRoutes(mCoordinates);
-        mRoutesAdapter.clear();
-        mRoutesAdapter.addAll(nearbyRoutes);
-        mRoutesAdapter.notifyDataSetChanged();
-        routesSpinner.setAdapter(mRoutesAdapter);
+        mDestLocation = new LatLng(geoPoint.getLatitude(), geoPoint.getLongitude());
     }
 
     @Override
@@ -369,9 +242,9 @@ public class SearchScheduleFragment extends Fragment implements OnStopTimesReady
         private final String toPlace;
         private String responseMessage = "";
 
-        RoutingTask(double fromPlaceLat, double fromPlaceLong, double toPlaceLat, double toPlaceLong) {
-            this.fromPlace = String.valueOf(fromPlaceLat) + "," + String.valueOf(fromPlaceLong);
-            this.toPlace = String.valueOf(toPlaceLat) + "," + String.valueOf(toPlaceLong);
+        RoutingTask(LatLng origCoords, LatLng destCoords) {
+            this.fromPlace = String.valueOf(origCoords.latitude) + "," + String.valueOf(origCoords.longitude);
+            this.toPlace = String.valueOf(destCoords.latitude) + "," + String.valueOf(destCoords.longitude);
         }
 
         @Override
@@ -440,30 +313,4 @@ public class SearchScheduleFragment extends Fragment implements OnStopTimesReady
             return responseMessage;
         }
     }
-
-    /**
-     * Shows the progress UI and hides the isSuccessfulLogin form.
-     */
-    /*@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    private void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-        }
-    }*/
 }
