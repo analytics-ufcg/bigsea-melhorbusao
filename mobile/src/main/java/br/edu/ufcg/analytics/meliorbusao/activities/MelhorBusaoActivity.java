2368bc12ca80f38aeda080285b54b4fd215c71a5
fonluiz <luiz.fonseca@ccc.ufcg.edu.br>
2017-05-08 14:24:19 -0300

    Fix bug that wouldn't permit the user sign in again to the app once they had sign out in certains circunstances.

--- mobile/src/main/java/br/edu/ufcg/analytics/meliorbusao/activities/MelhorBusaoActivity.java
+++ mobile/src/main/java/br/edu/ufcg/analytics/meliorbusao/activities/MelhorBusaoActivity.java
@@ -697,6 +697,7 @@ public class MelhorBusaoActivity extends AppCompatActivity
                     Intent intent = new Intent(getApplicationContext(), MelhorLoginActivity.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                     startActivity(intent);
+                    finish();
                 }
             }
         });
